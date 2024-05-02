6ab4801c8e3967df54e576f998952666c926831d
zahrahojati19 <129644190+zahrahojati19@users.noreply.github.com>
2024-05-02 10:20:36 +0330

    Ouldouz Functions Implementation P1

--- src/http/handler.go
+++ src/http/handler.go
@@ -6,16 +6,77 @@ import (
 )
 
 func (s *Server) setReservationHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
 
-	err := json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	var requestData map[string]interface{}
+	err := decoder.Decode(&requestData)
 	if err != nil {
+		http.Error(w, "Failed to parse request body", http.StatusBadRequest)
+		return
+	}
+
+	
+	eventID := requestData["EventID"].(string)
+	numTickets := int(requestData["numTickets"].(float64))
+
+	ticketIDs, err := s.BookingClient.BookTicket(eventID, numTickets)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	responseData := map[string]interface{}{
+		"ticketIDs": ticketIDs,
+	}
+	err = json.NewEncoder(w).Encode(responseData)
+	if err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *Server) getEventsHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := s.BookingClient.ShowAvailableEvents()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(events)
+	if err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
 	}
 }
 
 func (s *Server) createEventHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
 
-}
+	var eventData map[string]interface{}
+	err := decoder.Decode(&eventData)
+	if err != nil {
+		http.Error(w, "Failed to parse request body", http.StatusBadRequest)
+		return
+	}
 
-func (s *Server) getEventsHandler(w http.ResponseWriter, r *http.Request) {
+	name := eventData["Name"].(string)
+	data := eventData["Data"].(string)
+	totalTickets := int(eventData["totalTickets"].(float64))
 
+	event, err := s.BookingClient.CreateEvent(name, data, totalTickets)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	responseData := map[string]interface{}{
+		"EventID": event.ID,
+	}
+	err = json.NewEncoder(w).Encode(responseData)
+	if err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
